4eee775de8f61976d018f0e6b620288b89ab223c
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-12 21:08:11 +0600

    Update welcome.cpp

--- welcome.cpp
+++ welcome.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main ()
 {
   int a, b, c;
-  cin >> a >> b >> c;
+  cin >> a >> b >> c; // taking input from user 
   string s;
   cin >> s;
   cout << (a + b + c)<<" "<< s << endl;
